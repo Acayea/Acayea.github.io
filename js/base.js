fc576a76bfc23747ba931f1f7083a8d6bfb9a0f4
ari <a.cayea04@gmail.com>
2025-02-04 12:41:59 -0500

    auto push

---
+++ js/base.js
@@ -0,0 +1,15 @@
+document.write(`
+  <link rel="stylesheet" href="/css/index.css" />
+   <nav>
+    <ul>
+      <li><a href="/">home</a></li>
+      <li><a href="/webdev/index.html">webdev</a></li>
+      <li><a href="/pages/my-react-app/index.html">react</a></li>
+      <li><a href="/pages/about/about.html">about me</a></li>
+      <li><a href="">charts</a></li>
+      <li><a href="/pages/indexpage.html">index page </a></li>
+      <li><a href="/pages/quicklist.html">quick list </a></li>
+    </ul>
+  </nav>
+
+`);
---
+++ js/indexpage.js
@@ -0,0 +1,60 @@
+document.write(`
+    <ol type="A">
+      <li></li>
+      <br/>
+      <li></li>
+      <br />
+      <li></li>
+      <br />
+      <li></li>
+      <br />
+      <li></li>
+      <br />
+      <li></li>
+      <br />
+      <li></li>
+      <br />
+      <li><a href='/'>home</a></li>
+      <br />
+      <li></li>
+      <br />
+      <li></li>
+      <br />
+      <li></li>
+      <br />
+      <li></li>
+      <br />
+      <li></li>
+      <br />
+      <li></li>
+      <br />
+      <li></li>
+      <br />
+      <li></li>
+      <br />
+      <li></li>
+      <br />
+      <li></li>
+      <br />
+      <li></li>
+      <br />
+      <li></li>
+      <br />
+      <li></li>
+      <br />
+      <li></li>
+      <br />
+      <li></li>
+      <br />
+      <li></li>
+      <br />
+      <li></li>
+      <br />
+      <li></li>
+      <br />
+    </ol>
+
+
+
+
+    `);
